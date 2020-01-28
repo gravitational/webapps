6666d6866e6de04dfcade670768f37582a653de1
Lisa Kim <lisa@gravitational.com>
2020-01-28 14:50:16 -0800

    Create render helper for jest tests

---
+++ packages/design/src/utils/testing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  render as testingRender,
+  screen,
+  fireEvent,
+} from '@testing-library/react';
+import ThemeProvider from 'design/ThemeProvider';
+import theme from 'design/theme';
+
+export function render(component) {
+  return testingRender(
+    <ThemeProvider theme={theme}>{component}</ThemeProvider>
+  );
+}
+
+export { screen, fireEvent };
