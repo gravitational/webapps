b94c3afbf2bfeeb599b1469dfce25ac3b20353cf
Lisa Kim <lisa@gravitational.com>
2020-01-28 16:07:02 -0800

    Edit Topnav.test to use themeprovider helper

--- packages/design/src/TopNav/Topnav.test.js
+++ packages/design/src/TopNav/Topnav.test.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { TopNavComponent } from './TopNav.story';
-import ThemeProvider from 'design/ThemeProvider';
-import theme from 'design/theme';
+import { Sample } from './TopNav.story';
+import { render } from 'design/utils/testing';
 
-// TODO: find out how to setup a default rendering container (with ThemeProvider)
 describe('Design/TopNav', () => {
   it('should render', () => {
-    const { container } = render(
-      <ThemeProvider theme={theme}>
-        <TopNavComponent />
-      </ThemeProvider>
-    );
-
+    const { container } = render(<Sample />);
     expect(container.querySelectorAll('nav')).toHaveLength(1);
     expect(container.querySelectorAll('button')).toHaveLength(3);
   });
