81cc0be3c50ee13d63e75112fd7c70cafd68b121
Lisa Kim <lisa@gravitational.com>
2020-01-29 11:54:13 -0800

    Update testing.js to include helper to convert comp. to JSON

--- packages/design/src/utils/testing.js
+++ packages/design/src/utils/testing.js
@@ -4,6 +4,7 @@ import {
   screen,
   fireEvent,
 } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import ThemeProvider from 'design/ThemeProvider';
 import theme from 'design/theme';
 
@@ -11,6 +12,12 @@ export function render(component) {
   return testingRender(
     <ThemeProvider theme={theme}>{component}</ThemeProvider>
   );
+}
+
+export function convertToJSON(component) {
+  return renderer
+    .create(<ThemeProvider theme={theme}>{component}</ThemeProvider>)
+    .toJSON();
 }
 
 export { screen, fireEvent };
