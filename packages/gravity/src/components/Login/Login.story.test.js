1d9595957080162a865d7ea2d0fd46913dadda92
Lisa Kim <lisa@gravitational.com>
2020-01-29 18:35:43 -0800

    Remove unused helper and update snapshot to use jest-styled-components

--- packages/gravity/src/components/Login/Login.story.test.js
+++ packages/gravity/src/components/Login/Login.story.test.js
@@ -1,144 +1,404 @@
 import React from 'react';
 import { Basic, Success, Failed } from './Login.story';
-import { convertToJSON } from 'design/utils/testing';
+import { render } from 'design/utils/testing';
 
 describe('Gravity/Login.story', () => {
   it('basic login renders correctly', () => {
-    const tree = convertToJSON(<Basic />);
+    const { container } = render(<Basic />);
+    expect(container).toMatchInlineSnapshot(`
+      .c0 {
+        display: block;
+        outline: none;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        max-width: 200px;
+        max-height: 120px;
+      }
 
-    expect(tree).toMatchInlineSnapshot(`
-      Array [
+      .c2 {
+        box-sizing: border-box;
+        padding: 40px;
+      }
+
+      .c4 {
+        box-sizing: border-box;
+        margin-bottom: 24px;
+      }
+
+      .c7 {
+        line-height: 1.5;
+        margin: 0;
+        display: -webkit-inline-box;
+        display: -webkit-inline-flex;
+        display: -ms-inline-flexbox;
+        display: inline-flex;
+        -webkit-box-pack: center;
+        -webkit-justify-content: center;
+        -ms-flex-pack: center;
+        justify-content: center;
+        -webkit-align-items: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+        box-sizing: border-box;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        font-family: inherit;
+        font-weight: 600;
+        outline: none;
+        position: relative;
+        text-align: center;
+        -webkit-text-decoration: none;
+        text-decoration: none;
+        text-transform: uppercase;
+        -webkit-transition: all .3s;
+        transition: all .3s;
+        -webkit-font-smoothing: antialiased;
+        background: #00BFA5;
+        color: #FFFFFF;
+        min-height: 48px;
+        font-size: 14px;
+        padding: 0px 48px;
+        margin-top: 16px;
+        margin-bottom: 16px;
+        width: 100%;
+      }
+
+      .c7:active {
+        opacity: .56;
+      }
+
+      .c7::-moz-focus-inner {
+        border: 0;
+      }
+
+      .c7:hover,
+      .c7:focus {
+        background: #00EAC3;
+      }
+
+      .c7:active {
+        background: #26A69A;
+      }
+
+      .c7:disabled {
+        background: rgba(255,255,255,0.12);
+        color: rgba(255,255,255,0.3);
+      }
+
+      .c1 {
+        box-sizing: border-box;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 32px;
+        margin-bottom: 32px;
+        width: 456px;
+        background-color: #222C59;
+        box-shadow: 0 8px 32px rgba(0,0,0,0.24);
+        border-radius: 8px;
+      }
+
+      .c6 {
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        border-radius: 4px;
+        box-shadow: inset 0 2px 4px rgba(0,0,0,.24);
+        box-sizing: border-box;
+        border: none;
+        display: block;
+        height: 40px;
+        font-size: 16px;
+        padding: 12px 16px;
+        outline: none;
+        width: 100%;
+        color: rgba(0,0,0,0.87);
+        background-color: #FFFFFF;
+      }
+
+      .c6::-ms-clear {
+        display: none;
+      }
+
+      .c6::-webkit-input-placeholder {
+        opacity: 0.24;
+      }
+
+      .c6::-moz-placeholder {
+        opacity: 0.24;
+      }
+
+      .c6:-ms-input-placeholder {
+        opacity: 0.24;
+      }
+
+      .c6::placeholder {
+        opacity: 0.24;
+      }
+
+      .c5 {
+        color: #FFFFFF;
+        display: block;
+        font-size: 11px;
+        font-weight: 500;
+        text-transform: uppercase;
+        width: 100%;
+        margin-bottom: 4px;
+      }
+
+      .c3 {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        font-weight: 300;
+        font-size: 26px;
+        line-height: 40px;
+        margin: 0px;
+        margin-bottom: 16px;
+        color: #FFFFFF;
+        text-align: center;
+      }
+
+      <div>
         <img
-          className="sc-bdVaJa kaMnFS"
+          class="c0"
           src="file_stub"
-        />,
+        />
         <form
-          className="sc-htpNat sc-htoDjs giGZdH"
+          class="c1"
           width="456px"
         >
           <div
-            className="sc-htpNat kyeECs"
+            class="c2"
           >
             <div
-              className="sc-fjdhpX rqkSz"
+              class="c3"
               color="light"
             >
               Gravity
             </div>
             <div
-              className="sc-htpNat dfbocc"
+              class="c4"
             >
               <label
-                className="sc-gqjmRU gKhUOi"
-                fontSize={0}
+                class="c5"
+                font-size="0"
               >
                 Username
               </label>
               <input
-                autoComplete="off"
-                autoFocus={true}
-                className="sc-iwsKbI hMKqPV"
+                autocomplete="off"
+                class="c6"
                 color="text.onLight"
-                onChange={[Function]}
                 placeholder="User name"
                 value=""
               />
             </div>
             <div
-              className="sc-htpNat dfbocc"
+              class="c4"
             >
               <label
-                className="sc-gqjmRU gKhUOi"
-                fontSize={0}
+                class="c5"
+                font-size="0"
               >
                 Password
               </label>
               <input
-                autoComplete="off"
-                className="sc-iwsKbI hMKqPV"
+                autocomplete="off"
+                class="c6"
                 color="text.onLight"
-                onChange={[Function]}
                 placeholder="Password"
                 type="password"
                 value=""
               />
             </div>
             <button
-              className="sc-bxivhb hkyxUK"
+              class="c7"
               kind="primary"
-              onClick={[Function]}
-              size="large"
               type="submit"
               width="100%"
             >
               LOGIN
             </button>
           </div>
-        </form>,
-      ]
+        </form>
+      </div>
     `);
   });
 
   it('login success renders correctly', () => {
-    const tree = convertToJSON(<Success />);
-    expect(tree).toMatchInlineSnapshot(`
-      Array [
+    const { container } = render(<Success />);
+    expect(container).toMatchInlineSnapshot(`
+      .c0 {
+        display: block;
+        outline: none;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        max-width: 200px;
+        max-height: 120px;
+      }
+
+      .c2 {
+        display: inline-block;
+        -webkit-transition: color .3s;
+        transition: color .3s;
+        margin-bottom: 16px;
+        color: #00bfa5;
+        font-size: 64px;
+      }
+
+      .c1 {
+        box-sizing: border-box;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 24px;
+        margin-bottom: 24px;
+        padding: 48px;
+        width: 540px;
+        background-color: #222C59;
+        text-align: center;
+        box-shadow: 0 8px 32px rgba(0,0,0,0.24);
+        border-radius: 8px;
+      }
+
+      .c3 {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        font-weight: 300;
+        font-size: 34px;
+        line-height: 56px;
+        margin: 0px;
+        margin-bottom: 16px;
+      }
+
+      .c4 {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        font-weight: 300;
+        font-size: 16px;
+        line-height: 32px;
+        margin: 0px;
+      }
+
+      <div>
         <img
-          className="sc-bdVaJa kaMnFS"
+          class="c0"
           src="file_stub"
-        />,
+        />
         <div
-          className="sc-htpNat sc-htoDjs iFmlWo"
+          class="c1"
           width="540px"
         >
           <span
-            className="icon icon-checkmark-circle  sc-ifAKCX fntpuv"
+            class="icon icon-checkmark-circle  c2"
             color="success"
-            fontSize={64}
+            font-size="64"
           />
           <div
-            className="sc-fjdhpX bYLuAl"
+            class="c3"
           >
             Login Successful
           </div>
           <div
-            className="sc-fjdhpX gHGBk"
+            class="c4"
           >
             You have successfully signed into your account. You can close this window and continue using the product.
           </div>
-        </div>,
-      ]
+        </div>
+      </div>
     `);
   });
 
   it('failed login renders correctly', () => {
-    const tree = convertToJSON(<Failed />);
-    expect(tree).toMatchInlineSnapshot(`
-      Array [
+    const { container } = render(<Failed />);
+    expect(container).toMatchInlineSnapshot(`
+      .c0 {
+        display: block;
+        outline: none;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        max-width: 200px;
+        max-height: 120px;
+      }
+
+      .c1 {
+        box-sizing: border-box;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 32px;
+        width: 540px;
+        color: rgba(0,0,0,0.87);
+        background-color: #FFFFFF;
+        box-shadow: 0 8px 32px rgba(0,0,0,0.24);
+        border-radius: 8px;
+      }
+
+      .c4 {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        font-weight: 300;
+        font-size: 16px;
+        line-height: 32px;
+        margin: 0px;
+      }
+
+      .c2 {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        font-weight: 300;
+        font-size: 34px;
+        line-height: 56px;
+        margin: 0px;
+        margin-bottom: 16px;
+        text-align: center;
+      }
+
+      .c3 {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 16px;
+        margin: 0px;
+      }
+
+      .c5 {
+        color: #03a9f4;
+      }
+
+      <div>
         <img
-          className="sc-bdVaJa kaMnFS"
+          class="c0"
           src="file_stub"
-        />,
+        />
         <div
-          className="sc-htpNat sc-htoDjs jRHzQX"
+          class="c1"
           color="text.onLight"
           width="540px"
         >
           <div
-            className="sc-fjdhpX elIsj"
+            class="c2"
           >
             Login unsuccessful
           </div>
           <div>
              
             <div
-              className="sc-fjdhpX jdCZOL"
+              class="c3"
             >
               <div
-                className="sc-fjdhpX gHGBk"
+                class="c4"
               >
                 <a
-                  className="sc-eNQAEJ bQnGl"
+                  class="c5"
                   href="/web/login"
                 >
                   Please try again
@@ -147,8 +407,8 @@ describe('Gravity/Login.story', () => {
               </div>
             </div>
           </div>
-        </div>,
-      ]
+        </div>
+      </div>
     `);
   });
 });
