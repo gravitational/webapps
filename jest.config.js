b5207249091b6d915895ec200f9d0d0624b3a02a
Lisa Kim <lisa@gravitational.com>
2020-02-14 17:38:50 -0800

    Edit configurations

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 const config = require('@gravitational/build/jest/config');
 module.exports = {
   ...config,
-  collectCoverageFrom: ['**/packages/design/src/**/*.jsx'],
+  collectCoverageFrom: [
+    // '**/packages/design/src/**/*.jsx',
+    '**/packages/shared/components/**/*.jsx',
+  ],
   coverageReporters: ['text-summary', 'lcov'],
 };
