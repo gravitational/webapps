6b30c8653de3670b31449d6616e2be94f3065567
Lisa Kim <lisa@gravitational.com>
2020-02-21 18:53:59 -0800

    Unit test FieldInput

---
+++ packages/shared/components/FieldInput/FieldInput.test.js
@@ -0,0 +1,77 @@
+/**
+ * Copyright 2020 Gravitational, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import FieldInput from './FieldInput';
+import { render } from 'design/utils/testing';
+import { useRule } from '../Validation';
+import theme from '../../../design/src/theme';
+
+jest.mock('./../Validation/useRule');
+
+const mockFnRule = jest.fn().mockName('rule');
+const mockFnOnChange = jest.fn().mockName('onChange');
+const label = 'test label text renders';
+const placeholder = 'text to query input by';
+
+test('valid values and autofocus', () => {
+  // mock positive validation
+  useRule.mockImplementation(() => ({ valid: true, message: '' }));
+
+  const { getByText, getByPlaceholderText } = render(
+    <FieldInput
+      placeholder={placeholder}
+      autoFocus={true}
+      label={label}
+      rule={mockFnRule}
+      onChange={mockFnOnChange}
+    />
+  );
+
+  // test label is displayed
+  expect(getByText(label)).toBeInTheDocument();
+
+  // test autofocus focuses on input element
+  const inputEl = getByPlaceholderText(placeholder);
+  expect(document.activeElement).toEqual(inputEl);
+});
+
+test('invalid value renders err msg and colors', () => {
+  const errMsg = 'some error message';
+  const errorColor = theme.colors.error.main;
+
+  // mock negative validation
+  useRule.mockImplementation(() => ({ valid: false, message: errMsg }));
+
+  const { getByText, getByPlaceholderText } = render(
+    <FieldInput
+      placeholder={placeholder}
+      label={label}
+      rule={mockFnRule}
+      onChange={mockFnOnChange}
+    />
+  );
+
+  // test !valid values renders with error message
+  const labelEl = getByText(errMsg);
+  expect(labelEl).toHaveStyle({ color: errorColor });
+
+  // test !valid values renders error colors
+  const inputEl = getByPlaceholderText(placeholder);
+  expect(inputEl).toHaveStyle({
+    'border-color': errorColor,
+  });
+});
