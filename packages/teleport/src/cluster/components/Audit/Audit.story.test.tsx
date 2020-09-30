7afbd5c7cc9556bc0d518726a06d958fd296c123
Lisa Kim <lisa@gravitational.com>
2020-09-29 17:11:02 -0700

    Add snapshot

--- packages/teleport/src/cluster/components/Audit/Audit.story.test.tsx
+++ packages/teleport/src/cluster/components/Audit/Audit.story.test.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Overflow } from './Audit.story';
+import { Overflow, AuditLog } from './Audit.story';
 import { render } from 'design/utils/testing';
 
 test('overflow', async () => {
   const { container, findByText } = render(<Overflow />);
   await findByText(/exceeded the maximum limit of 9999 events/);
+
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test('null response still renders table', async () => {
+  const { container, findByText } = render(<AuditLog />);
+  await findByText(/no data available/i);
 
   expect(container.firstChild).toMatchSnapshot();
 });
