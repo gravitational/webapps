94fb605f6ccc837522f0279863295a0e531b8a05
Lisa Kim <lisa@goteleport.com>
2021-02-25 12:46:13 -0800

    Revert "Create readonly editor sessions"
    This reverts commit bb67e93558b7830d2487f170770ff4ca193647d7.

--- packages/shared/components/TextEditor/TextEditor.jsx
+++ packages/shared/components/TextEditor/TextEditor.jsx
@@ -37,6 +37,10 @@ class TextEditor extends React.Component {
     }
   };
 
+  getData() {
+    return this.sessions.map(s => s.getValue());
+  }
+
   componentDidUpdate(prevProps) {
     if (prevProps.activeIndex !== this.props.activeIndex) {
       this.setActiveSession(this.props.activeIndex);
@@ -46,23 +50,14 @@ class TextEditor extends React.Component {
   }
 
   createSession({ content, type, tabSize = 2 }) {
-    const session = new ace.EditSession(content);
-    const undoManager = new UndoManager();
+    const mode = getMode(type);
+    let session = new ace.EditSession(content);
+    let undoManager = new UndoManager();
     undoManager.markClean();
     session.setUndoManager(undoManager);
     session.setUseWrapMode(false);
+    session.setMode(mode);
     session.setOptions({ tabSize, useSoftTabs: true });
-    session.setMode(getMode(type));
-    return session;
-  }
-
-  createSessionReadOnly({ content, type }) {
-    const session = new ace.EditSession(content);
-
-    // Workers are syntax validators which isn't needed in read only mode.
-    session.setOptions({ useWorker: false });
-    session.setUseWrapMode(false);
-    session.setMode(getMode(type));
     return session;
   }
 
@@ -77,12 +72,8 @@ class TextEditor extends React.Component {
   }
 
   initSessions(data = []) {
+    this.isDirty = false;
     this.sessions = data.map(item => this.createSession(item));
-    this.setActiveSession(0);
-  }
-
-  initSessionsReadOnly(data = []) {
-    this.sessions = data.map(item => this.createSessionReadOnly(item));
     this.setActiveSession(0);
   }
 
@@ -99,13 +90,8 @@ class TextEditor extends React.Component {
     this.editor.on('input', this.onChange);
     this.editor.setReadOnly(readOnly);
     this.editor.setTheme(theme);
-
-    if (readOnly) {
-      this.initSessionsReadOnly(data);
-      return;
-    }
-
     this.initSessions(data);
+    this.editor.focus();
   }
 
   componentWillUnmount() {
