06b97730b5262a5d527464d8c48c3466b3c438f0
Amha Desta <amha@goteleport.com>
2021-04-27 17:03:39 -0500

    Restructure files

--- packages/teleport/src/services/kube/index.ts
+++ packages/teleport/src/services/kube/index.ts
@@ -14,4 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import service from './kubernetes';
+
 export * from './types';
+export default service;
