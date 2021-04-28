cd457aba4ce1419d7eca496ad8135740d511a025
Yassine Bounekhla <yassine@goteleport.com>
2021-04-28 13:24:03 -0400

    :white_check_mark: Add unit test

---
+++ packages/teleport/src/services/database/databases.test.ts
@@ -0,0 +1,53 @@
+/*
+Copyright 2021 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import DatabaseService from './databases';
+import api from 'teleport/services/api';
+
+test('correct formatting of database fetch response', async () => {
+  jest.spyOn(api, 'get').mockResolvedValue(mockResponse);
+
+  const database = new DatabaseService();
+  const response = await database.fetchDatabases('im-a-cluster');
+
+  expect(response).toEqual([
+    {
+      name: 'aurora',
+      desc: 'PostgreSQL 11.6: AWS Aurora',
+      type: 'RDS/PostgreSQL',
+      uri:
+        'postgres-aurora-instance-1.c1xpjrob56xs.us-west-1.rds.amazonaws.com:5432',
+      tags: ['cluster: root', 'env: aws'],
+    },
+  ]);
+});
+
+const mockResponse = {
+  items: [
+    {
+      name: 'aurora',
+      desc: 'PostgreSQL 11.6: AWS Aurora',
+      protocol: 'postgres',
+      type: 'rds',
+      uri:
+        'postgres-aurora-instance-1.c1xpjrob56xs.us-west-1.rds.amazonaws.com:5432',
+      labels: [
+        { name: 'cluster', value: 'root' },
+        { name: 'env', value: 'aws' },
+      ],
+    },
+  ],
+};
