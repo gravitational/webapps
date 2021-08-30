0279555ccf518108de4374ab49169fb19ea98228
Isaiah Becker-Mayer <isaiah@goteleport.com>
2021-08-30 14:22:38 -0700

    First draft of a system which can pop up a new desktop session and render desktop screen to canvas

--- packages/teleport/src/Teleport.tsx
+++ packages/teleport/src/Teleport.tsx
@@ -28,6 +28,7 @@ import Invite, { ResetPassword } from './Invite';
 import Login, { LoginSuccess, LoginFailed } from './Login';
 import AppLauncher from './AppLauncher';
 import Console from './Console';
+import DesktopSession from './DesktopSession';
 import Player from './Player';
 import TeleportContextProvider from './teleportContextProvider';
 import TeleportContext from './teleportContext';
@@ -97,7 +98,7 @@ function renderAuthenticated(children) {
 
   return (
     <Switch>
-      <Route path={cfg.routes.desktop} component={Console} />
+      <Route path={cfg.routes.desktop} component={DesktopSession} />
       <Route path={cfg.routes.console} component={Console} />
       <Route path={cfg.routes.player} component={Player} />
       <Route path={cfg.routes.root} component={Main} />
