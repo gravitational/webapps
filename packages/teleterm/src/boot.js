f82061e2f38181e5cb9cd6a865a43ee82a85d8ca
Alexey Kontsevoy <biz.kovoy@gmail.com>
2021-10-06 11:31:46 -0400

    fixes

--- packages/teleterm/src/boot.js
+++ packages/teleterm/src/boot.js
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import UI from './ui';
+import Ui from './ui';
 import { createMemoryHistory } from 'history';
 
 const history = createMemoryHistory();
 
-ReactDOM.render(<UI history={history} />, document.getElementById('app'));
+ReactDOM.render(<Ui history={history} />, document.getElementById('app'));
