d82b67ece6551e46de5f2187d23f387996845f5a
Grzegorz Zdunek <gzdunek@users.noreply.github.com>
2021-11-17 17:55:02 +0100

    Add logging to file (#455)

--- packages/teleterm/src/boot.tsx
+++ packages/teleterm/src/boot.tsx
@@ -9,4 +9,16 @@ const appContext = new AppContext(electronGlobals);
 
 appContext.init();
 
+const globalLogger = electronGlobals.createLogger('Uncaught Error');
+
+// global handler for uncaught errors
+window.addEventListener('error', event => {
+  globalLogger.error(event.error.stack);
+});
+
+// global handler for uncaught promise rejections
+window.addEventListener('unhandledrejection', event => {
+  globalLogger.error(event.reason.stack);
+});
+
 ReactDOM.render(<Ui ctx={appContext} />, document.getElementById('app'));
