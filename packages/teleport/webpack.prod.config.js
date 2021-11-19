57d40fe945d25ee365d6be30e51501a627ee2bf5
Yassine Bounekhla <yassine@goteleport.com>
2021-11-19 13:10:28 +0100

    remove need for extend.js

--- packages/teleport/webpack.prod.config.js
+++ packages/teleport/webpack.prod.config.js
@@ -1,4 +1,16 @@
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const createBaseDefaults = require('@gravitational/build/webpack/webpack.base');
 const defaultCfg = require('@gravitational/build/webpack/webpack.prod.config');
-const extend = require('./webpack.extend');
 
-module.exports = extend(defaultCfg);
+module.exports = {
+  ...defaultCfg,
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.HashedModuleIdsPlugin(),
+    createBaseDefaults().plugins.createIndexHtml({
+      favicon: path.join(__dirname, '/src/favicon.ico'),
+    }),
+  ],
+};
