378db3c615f8706c5b40b13402bb25d66bc9cf2b
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2021-12-03 19:48:48 +0100

    arrange the cards evenly depending on the available space

--- packages/teleterm/src/ui/Tabs/TabItem.tsx
+++ packages/teleterm/src/ui/Tabs/TabItem.tsx
@@ -1,18 +1,15 @@
 /*
-Copyright 2019 Gravitational, Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ Copyright 2019 Gravitational, Inc.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 import React from 'react';
 import styled from 'styled-components';
@@ -24,10 +21,8 @@ export default function TabItem(props: Props) {
   const { name, active, onClick, onClose, style } = props;
   return (
     <StyledTabItem alignItems="center" active={active} style={style}>
-      <StyledTabButton onClick={onClick}>
-        <Text mx="auto" title={name}>
-          {name}
-        </Text>
+      <StyledTabButton onClick={onClick} title={name}>
+        <Text mx="auto">{name}</Text>
       </StyledTabButton>
       <StyledCloseButton title="Close" onClick={onClose}>
         <CloseIcon />
@@ -69,28 +64,26 @@ function fromProps({ theme, active }) {
 }
 
 const StyledTabItem = styled(Flex)`
-  max-width: 200px;
+  min-width: 0;
   height: 100%;
   ${fromProps}
+  &:hover {
+    background: ${props => props.theme.colors.primary.light};
+  }
 `;
 
 const StyledTabButton = styled.button`
   display: flex;
-  flex: 1;
-  align-items: center;
   cursor: pointer;
-  text-decoration: none;
   outline: none;
-  margin: 0;
-  text-decoration: none;
   color: inherit;
   line-height: 32px;
   background-color: transparent;
   white-space: nowrap;
-  overflow: hidden;
-  padding: 0 16px;
-  text-overflow: ellipsis;
+  padding: 0 8px;
   border: none;
+  min-width: 0;
+  width: 100%;
 `;
 
 const StyledCloseButton = styled.button`
@@ -104,7 +97,6 @@ const StyledCloseButton = styled.button`
   padding: 0;
   margin: 0 8px 0 0;
   transition: all 0.3s;
-
   &:hover {
     background: ${props => props.theme.colors.danger};
   }
--- packages/teleterm/src/ui/Tabs/Tabs.tsx
+++ packages/teleterm/src/ui/Tabs/Tabs.tsx
@@ -1,18 +1,15 @@
 /*
-Copyright 2019 Gravitational, Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ Copyright 2019 Gravitational, Inc.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 import React from 'react';
 import styled from 'styled-components';
@@ -66,7 +63,7 @@ export function Tabs(props: Props) {
       bold
       {...styledProps}
     >
-      {$items}
+      <FullWidthContainer>{$items}</FullWidthContainer>
       {$items.length > 0 && (
         <ButtonIcon
           ml="2"
@@ -91,13 +88,19 @@ type Props = {
   [index: string]: any;
 };
 
-const StyledTabs = styled(Box)`
-  min-height: 32px;
-  border-radius: 4px;
+const FullWidthContainer = styled.div`
   display: flex;
-  flex-wrap: no-wrap;
-  align-items: center;
-  flex-shrink: 0;
-  overflow: hidden;
-  ${typography}
+  min-width: 0;
+  width: 100%;
 `;
+
+const StyledTabs = styled(Box)`
+   min-height: 32px;
+   border-radius: 4px;
+   display: flex;
+   flex-wrap: nowrap;
+   align-items: center;
+   flex-shrink: 0;
+   overflow: hidden;
+   ${typography}
+ `;
