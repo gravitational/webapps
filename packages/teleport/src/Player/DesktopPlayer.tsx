bb098e812dde957a835b684e96d82163f6449e7b
Isaiah Becker-Mayer <isaiah@goteleport.com>
2022-01-21 12:33:51 -0500

    Puts TdpClientCanvas into the DesktopPlayer

--- packages/teleport/src/Player/DesktopPlayer.tsx
+++ packages/teleport/src/Player/DesktopPlayer.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import { Flex } from 'design';
 import ProgressBar from './ProgressBar';
 import { PlayerClient } from 'teleport/lib/tdp/playerClient';
 import cfg from 'teleport/config';
 import { getAccessToken, getHostName } from 'teleport/services/api';
+import TdpClientCanvas from 'teleport/components/TdpClientCanvas';
+import { ImageFragment } from 'teleport/lib/tdp/client';
 
 export const DesktopPlayer = ({
   sid,
@@ -13,13 +15,6 @@ export const DesktopPlayer = ({
   sid: string;
   clusterId: string;
 }) => {
-  console.log(
-    cfg.api.desktopPlaybackWsAddr
-      .replace(':fqdm', getHostName())
-      .replace(':clusterId', clusterId)
-      .replace(':sid', sid)
-      .replace(':token', getAccessToken())
-  );
   const playerClient = new PlayerClient(
     cfg.api.desktopPlaybackWsAddr
       .replace(':fqdm', getHostName())
@@ -28,7 +23,7 @@ export const DesktopPlayer = ({
       .replace(':token', getAccessToken())
   );
 
-  playerClient.init();
+  const firstImageFragmentRef = useRef(true);
 
   const state = {
     max: 500, // total time of the recording in ms
@@ -46,9 +41,36 @@ export const DesktopPlayer = ({
 
   return (
     <StyledPlayer>
-      <Flex flex="1" flexDirection="column" overflow="auto">
-        Desktop Player
-      </Flex>
+      <TdpClientCanvas
+        tdpCli={playerClient}
+        tdpCliOnImageFragment={(
+          ctx: CanvasRenderingContext2D,
+          data: ImageFragment
+        ) => {
+          if (firstImageFragmentRef.current) {
+            ctx.canvas.width = ctx.canvas.clientWidth;
+            ctx.canvas.height = ctx.canvas.clientHeight;
+            firstImageFragmentRef.current = false;
+          }
+          ctx.drawImage(data.image, data.left, data.top);
+        }}
+        tdpCliOnTdpError={(err: Error) => {}} // TODO: we should show an error
+        tdpCliOnWsClose={() => {}} // TODO: restart?
+        tdpCliOnWsOpen={() => {}}
+        onKeyDown={(cli: PlayerClient, e: KeyboardEvent) => {}} // TODO: make these nullable and modify TdpClientCanvas so as not to prevent defaults?
+        onKeyUp={(cli: PlayerClient, e: KeyboardEvent) => {}}
+        onMouseMove={(
+          cli: PlayerClient,
+          canvas: HTMLCanvasElement,
+          e: MouseEvent
+        ) => {}}
+        onMouseDown={(cli: PlayerClient, e: MouseEvent) => {}}
+        onMouseUp={(cli: PlayerClient, e: MouseEvent) => {}}
+        onMouseWheelScroll={(cli: PlayerClient, e: WheelEvent) => {}}
+        // overflow: 'hidden' is needed to prevent the canvas from outgrowing the container due to some weird css flex idiosyncracy.
+        // See https://gaurav5430.medium.com/css-flex-positioning-gotchas-child-expands-to-more-than-the-width-allowed-by-the-parent-799c37428dd6.
+        style={{ display: 'flex', flexGrow: 1, overflow: 'hidden' }}
+      />
       <ProgressBar {...state} />
     </StyledPlayer>
   );
@@ -56,10 +78,7 @@ export const DesktopPlayer = ({
 
 const StyledPlayer = styled.div`
   display: flex;
-  height: 100%;
-  width: 100%;
-  position: absolute;
   flex-direction: column;
-  flex: 1;
-  justify-content: space-between;
+  width: 100%;
+  height: 100%;
 `;
