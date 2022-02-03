e0ae124b70d9e47cec70bb3bfe8b43c443971fc6
Isaiah Becker-Mayer <isaiah@goteleport.com>
2022-02-02 21:00:59 -0500

    typing fix and changes if else-if else to switch

--- packages/teleport/src/lib/tdp/client.ts
+++ packages/teleport/src/lib/tdp/client.ts
@@ -81,18 +81,24 @@ export default class Client extends EventEmitter {
   processMessage(buffer: ArrayBuffer) {
     const messageType = this.codec._decodeMessageType(buffer);
     try {
-      if (messageType === MessageType.PNG_FRAME) {
-        this.handlePngFrame(buffer);
-      } else if (messageType === MessageType.CLIENT_SCREEN_SPEC) {
-        this.handleClientScreenSpec(buffer);
-      } else if (messageType === MessageType.MOUSE_BUTTON) {
-        this.handleMouseButton(buffer);
-      } else if (messageType === MessageType.MOUSE_MOVE) {
-        this.handleMouseMove(buffer);
-      } else if (messageType === MessageType.ERROR) {
-        this.handleError(new Error(this.codec.decodeErrorMessage(buffer)));
-      } else {
-        this.logger.warn(`received unsupported message type ${messageType}`);
+      switch (messageType) {
+        case MessageType.PNG_FRAME:
+          this.handlePngFrame(buffer);
+          break;
+        case MessageType.CLIENT_SCREEN_SPEC:
+          this.handleClientScreenSpec(buffer);
+          break;
+        case MessageType.MOUSE_BUTTON:
+          this.handleMouseButton(buffer);
+          break;
+        case MessageType.MOUSE_MOVE:
+          this.handleMouseMove(buffer);
+          break;
+        case MessageType.ERROR:
+          this.handleError(new Error(this.codec.decodeErrorMessage(buffer)));
+          break;
+        default:
+          this.logger.warn(`received unsupported message type ${messageType}`);
       }
     } catch (err) {
       this.handleError(err);
