66182c8400300f1eded4c051b99d449141aa0236
Isaiah Becker-Mayer <isaiah@goteleport.com>
2022-02-16 14:02:09 -0500

    fixes test

--- packages/teleport/src/services/user/user.test.ts
+++ packages/teleport/src/services/user/user.test.ts
@@ -142,7 +142,8 @@ test('undefined values in context response gives proper default values', async (
         create: false,
         remove: false,
       },
-      clipboard: true,
+      canShareClipboard: true,
+      desktopSessionRecording: true,
     },
     cluster: {
       clusterId: 'aws',
