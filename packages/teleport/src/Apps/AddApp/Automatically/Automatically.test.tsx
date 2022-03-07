4305f75d8bbacb7df355b037632b49cf7132c0ec
matheus <matheus.battirola@goteleport.com>
2022-03-07 13:33:32 -0300

    Add test case to automatic app add

---
+++ packages/teleport/src/Apps/AddApp/Automatically/Automatically.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { fireEvent, render, screen } from 'design/utils/testing';
+import Automatically from './Automatically';
+
+test('render command only after form submit', async () => {
+  const cmd = 'sudo some-command';
+  render(
+    <Automatically
+      cmd={cmd}
+      attempt={{ status: 'success' }}
+      onClose={() => {}}
+      onCreate={() => Promise.resolve(true)}
+      expires=""
+    />
+  );
+
+  // initially, should not show the command
+  expect(screen.queryByText(cmd)).toBeNull();
+
+  // set app name
+  const appNameInput = screen.getByPlaceholderText('jenkins');
+  fireEvent.change(appNameInput, { target: { value: 'app-name' } });
+
+  // set app url
+  const appUriInput = screen.getByPlaceholderText('https://localhost:4000');
+  fireEvent.change(appUriInput, {
+    target: { value: 'https://gravitational.com' },
+  });
+
+  // click button
+  screen.getByRole('button', { name: /Generate Script/i }).click();
+
+  // after form submissionm should show the command
+  expect(screen.queryByText(cmd)).not.toBeNull();
+});
