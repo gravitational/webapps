ce85a022a96dbc68d66cf4c6554deaa1911908e6
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2022-03-14 14:30:23 +0100

    add keyboard shortcut to toggle `Identity` popover

--- packages/teleterm/src/ui/TopBar/Identity/Identity.tsx
+++ packages/teleterm/src/ui/TopBar/Identity/Identity.tsx
@@ -1,8 +1,9 @@
-import React, { useRef, useState } from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
 import Popover from 'design/Popover';
 import { useIdentity } from './useIdentity';
 import { IdentityList } from './IdentityList/IdentityList';
 import { IdentitySelector } from './IdentitySelector/IdentitySelector';
+import { useKeyboardShortcuts } from 'teleterm/ui/services/keyboardShortcuts';
 
 export function Identity() {
   const selectorRef = useRef<HTMLButtonElement>();
@@ -16,12 +17,25 @@ export function Identity() {
     addCluster,
   } = useIdentity();
 
+  const togglePopover = useCallback(() => {
+    setIsPopoverOpened(wasOpened => !wasOpened);
+  }, [setIsPopoverOpened]);
+
+  useKeyboardShortcuts(
+    useMemo(
+      () => ({
+        'toggle-identity': togglePopover,
+      }),
+      [togglePopover]
+    )
+  );
+
   const loggedInUser = activeRootCluster?.loggedInUser;
   return (
     <>
       <IdentitySelector
         ref={selectorRef}
-        onClick={() => setIsPopoverOpened(wasOpened => !wasOpened)}
+        onClick={togglePopover}
         isOpened={isPopoverOpened}
         userName={loggedInUser?.name}
         hostName={activeRootCluster?.name}
@@ -32,6 +46,7 @@ export function Identity() {
         anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
         onClose={() => setIsPopoverOpened(false)}
       >
+        {focusGrabber}
         <IdentityList
           loggedInUser={loggedInUser}
           clusters={rootClusters}
@@ -44,3 +59,18 @@ export function Identity() {
     </>
   );
 }
+
+// Hack - for some reason xterm.js doesn't allow moving a focus to the Identity popover
+// when it is focused using element.focus(). Moreover, it looks like this solution has a benefit
+// of returning the focus to the previously focused element when popover is closed.
+const focusGrabber = (
+  <input
+    style={{
+      opacity: 0,
+      position: 'absolute',
+      height: 0,
+      zIndex: -1,
+    }}
+    autoFocus={true}
+  />
+);
