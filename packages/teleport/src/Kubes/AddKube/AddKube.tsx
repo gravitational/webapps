5448ba809e1fb3586e1bd8c12266befe10e255a9
matheus <matheus.battirola@goteleport.com>
2022-04-04 14:12:27 -0300

    Improve style and add processing and failed statuses handle

--- packages/teleport/src/Kubes/AddKube/AddKube.tsx
+++ packages/teleport/src/Kubes/AddKube/AddKube.tsx
@@ -28,15 +28,25 @@ import Validation, { Validator } from 'shared/components/Validation';
 import { Question } from 'design/Icon';
 import Box from 'design/Box';
 import Text from 'design/Text';
-import useAddKube from './useAddKube';
+import useAddKube, { State } from './useAddKube';
 import useTeleport from 'teleport/useTeleport';
+import Alert from 'design/Alert';
 
-export function AddKube({ onClose }: Props) {
+export default function Container(props: Props) {
+  const ctx = useTeleport();
+  const state = useAddKube(ctx);
+  return <AddKube {...state} {...props} />;
+}
+
+export function AddKube({
+  onClose,
+  attempt,
+  createToken,
+  expires,
+  token,
+}: Props & State) {
   const { hostname, port } = window.document.location;
   const host = `${hostname}:${port || '443'}`;
-
-  const ctx = useTeleport();
-  const { attempt, createToken, expires, token } = useAddKube(ctx);
 
   const [cmd, setCmd] = useState('');
   const [namespace, setNamespace] = useState('');
@@ -67,106 +77,109 @@ export function AddKube({ onClose }: Props) {
         minHeight: '328px',
       })}
       disableEscapeKeyDown={false}
-      // onClose={onClose}
+      onClose={onClose}
       open={true}
     >
-      <Flex flex="1" flexDirection="column">
-        <DialogTitle mr="auto" mb="4">
-          Add Kubernetes
-        </DialogTitle>
-        <DialogContent minHeight="254px" flex="0 0 auto">
-          <Box mb={4}>
-            Install Teleport Agent in your cluster via Helm to easily connect
-            your Kubernetes cluster with Teleport.
-          </Box>
-          <Box mb={4}>
-            <Text>
-              <Text bold as="span">
-                Step 1
-              </Text>
-              {' - Add teleport-agent chart to charts repository'}
-            </Text>
-            <TextSelectCopy
-              text={
-                'helm repo add teleport https://charts.releases.teleport.dev && helm repo update'
-              }
-            />
-          </Box>
-          <Box mb={4}>
-            <Text bold as="span">
-              Step 2
-            </Text>
-            {' - Enter your cluster information'}
-            <Validation>
-              {({ validator }) => (
-                <>
-                  <Flex alignItems="center" flexDirection="row">
-                    <FieldInput
-                      mb={2}
-                      // rule={requiredAppName}
-                      label="Namespace"
-                      autoFocus
-                      value={namespace}
-                      placeholder="teleport"
-                      width="320px"
-                      mr="3"
-                      // onKeyPress={e => handleEnterPress(e, validator)}
-                      onChange={e => setNamespace(e.target.value)}
-                      labelIcon={
-                        <Question
-                          style={{ margin: '0 0 4px 4px' }}
-                          title="Information"
-                        />
-                      }
-                    />
-                    <FieldInput
-                      mb={2}
-                      // rule={requiredAppName}
-                      label="Kubernetes Cluster Name"
-                      autoFocus
-                      value={clusterName}
-                      placeholder="my-cluster"
-                      width="320px"
-                      mr="3"
-                      // onKeyPress={e => handleEnterPress(e, validator)}
-                      onChange={e => setClusterName(e.target.value)}
-                      labelIcon={
-                        <Question
-                          style={{ margin: '0 0 4px 4px' }}
-                          title="Information"
-                        />
-                      }
-                    />
-                  </Flex>
-
-                  <ButtonPrimary
-                    // disabled={attempt.status === 'processing'}
-                    onClick={() => handleGenerate(validator)}
-                  >
-                    Generate Script
-                  </ButtonPrimary>
-                </>
-              )}
-            </Validation>
-          </Box>
-          {cmd && (
-            <Box>
-              <Text mt={3}>Step 3: Install Kubernetes agent.</Text>
-              <Text mt="1">
-                The token will be valid for{' '}
-                <Text bold as={'span'}>
-                  {expires}.
+      <Validation>
+        {({ validator }) => (
+          <Flex flex="1" flexDirection="column">
+            <DialogTitle mr="auto" mb="4">
+              Add Kubernetes
+            </DialogTitle>
+            {attempt.status == 'failed' && (
+              <Alert kind="danger" children={attempt.statusText} />
+            )}
+            <DialogContent minHeight="254px" flex="0 0 auto">
+              <Box mb={4}>
+                Install Teleport Agent in your cluster via Helm to easily
+                connect your Kubernetes cluster with Teleport.
+              </Box>
+              <Box mb={4}>
+                <Text>
+                  <Text bold as="span">
+                    Step 1
+                  </Text>
+                  {' - Add teleport-agent chart to charts repository'}
                 </Text>
-              </Text>
-              <TextSelectCopy text={cmd} mb={2} />
-              <Text>Tip: save the YAML file to apply updates later</Text>
-            </Box>
-          )}
-        </DialogContent>
-        <DialogFooter>
-          <ButtonSecondary onClick={onClose}>Close</ButtonSecondary>
-        </DialogFooter>
-      </Flex>
+                <TextSelectCopy
+                  text={
+                    'helm repo add teleport https://charts.releases.teleport.dev && helm repo update'
+                  }
+                />
+              </Box>
+              <Box mb={4}>
+                <Text bold as="span">
+                  Step 2
+                </Text>
+                {' - Enter your cluster information'}
+
+                <Flex alignItems="center" flexDirection="row">
+                  <FieldInput
+                    mb={2}
+                    // rule={requiredAppName}
+                    label="Namespace"
+                    autoFocus
+                    value={namespace}
+                    placeholder="teleport"
+                    width="320px"
+                    mr="3"
+                    // onKeyPress={e => handleEnterPress(e, validator)}
+                    onChange={e => setNamespace(e.target.value)}
+                    labelIcon={
+                      <Question
+                        style={{ margin: '0 0 4px 4px' }}
+                        title="Information"
+                      />
+                    }
+                  />
+                  <FieldInput
+                    mb={2}
+                    // rule={requiredAppName}
+                    label="Kubernetes Cluster Name"
+                    autoFocus
+                    value={clusterName}
+                    placeholder="my-cluster"
+                    width="320px"
+                    mr="3"
+                    // onKeyPress={e => handleEnterPress(e, validator)}
+                    onChange={e => setClusterName(e.target.value)}
+                    labelIcon={
+                      <Question
+                        style={{ margin: '0 0 4px 4px' }}
+                        title="Information"
+                      />
+                    }
+                  />
+                </Flex>
+              </Box>
+              {cmd && (
+                <Box>
+                  <Text mt="3" mb="2" bold typography="h5">
+                    Script Generated
+                  </Text>
+                  <Text mt="1">
+                    The token will be valid for{' '}
+                    <Text bold as={'span'}>
+                      {expires}.
+                    </Text>
+                  </Text>
+                  <TextSelectCopy text={cmd} mb={2} />
+                  <Text>Tip: save the YAML file to apply updates later</Text>
+                </Box>
+              )}
+            </DialogContent>
+            <DialogFooter>
+              <ButtonPrimary
+                disabled={attempt.status === 'processing'}
+                onClick={() => handleGenerate(validator)}
+              >
+                {cmd ? 'Regenerate Script' : 'Generate Script'}
+              </ButtonPrimary>
+              <ButtonSecondary onClick={onClose}>Close</ButtonSecondary>
+            </DialogFooter>
+          </Flex>
+        )}
+      </Validation>
     </Dialog>
   );
 }
@@ -192,5 +205,3 @@ helm install -f prod-cluster-values.yaml --create-namespace --namespace ${namesp
 type Props = {
   onClose(): void;
 };
-
-export default AddKube;
