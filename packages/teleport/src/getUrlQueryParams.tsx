7f8714b70a4284a588ff82e3af566fa0763952ca
Yassine Bounekhla <yassine@goteleport.com>
2022-05-04 13:32:26 -0400

    add initial sorting

--- packages/teleport/src/getUrlQueryParams.tsx
+++ packages/teleport/src/getUrlQueryParams.tsx
@@ -24,15 +24,28 @@ export default function getResourceUrlQueryParams(
   const search = searchParams.get('search');
   const sort = searchParams.get('sort');
 
-  return {
-    query,
-    search,
+  console.log({
     sort: sort
       ? ({
           fieldName: sort.split(':')[0],
           dir: sort.split(':')[1]?.toUpperCase() || 'ASC',
         } as SortType)
       : null,
+  });
+
+  // Converts the "fieldname:dir" format into {fieldName: "", dir: ""}
+  const processedSortParam = sort
+    ? ({
+        fieldName: sort.split(':')[0],
+        dir: sort.split(':')[1]?.toUpperCase() || 'ASC',
+      } as SortType)
+    : null;
+
+  return {
+    query,
+    search,
+    // Conditionally adds the sort field based on whether it exists or not
+    ...(!!processedSortParam && { sort: processedSortParam }),
   };
 }
 
