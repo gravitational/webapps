fa37504c33bd338aee213fe2b1403ec6156569dd
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2022-05-31 10:16:45 +0200

    Remove `teleterm/ui/logger` and use only `teleterm/logger`

--- packages/teleterm/src/ui/logger.ts
+++
@@ -1,8 +0,0 @@
-import Logger from 'teleterm/logger';
-import { ElectronGlobals } from 'teleterm/types';
-
-export default Logger;
-
-export function initLogger(globals: ElectronGlobals) {
-  Logger.init(globals.loggerService);
-}
