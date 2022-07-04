c7b841635c7a988d1009475ccad2c9e1c34cf685
matheus <matheus.battirola@goteleport.com>
2022-07-04 17:12:52 -0300

    Call upgrade schedule modal from Clusters

--- packages/teleport/src/Clusters/Clusters.tsx
+++ packages/teleport/src/Clusters/Clusters.tsx
@@ -20,24 +20,50 @@ import {
   FeatureHeaderTitle,
 } from 'teleport/components/Layout';
 import React from 'react';
-import { Box, Indicator } from 'design';
+import { Box, ButtonPrimary, Indicator } from 'design';
 import { Danger } from 'design/Alert';
 import useTeleport from 'teleport/useTeleport';
 import ClusterList from './ClusterList';
 import useClusters from './useClusters';
+import { Clock } from 'design/Icon';
+import { ScheduleUpgrades } from './ScheduleUpgrades/ScheduleUpgrades';
+import useUpgradeWindows from './useUpgradeWindows';
 
 export default function Container() {
   const ctx = useTeleport();
   const state = useClusters(ctx);
-  return <Clusters {...state} />;
+  const upgradeWindowsState = useUpgradeWindows(ctx);
+  return <Clusters {...state} {...upgradeWindowsState} />;
 }
 
-export function Clusters(props: ReturnType<typeof useClusters>) {
-  const { clusters, enabledFeatures, initAttempt } = props;
+type State = ReturnType<typeof useClusters> &
+  ReturnType<typeof useUpgradeWindows>;
+
+export function Clusters(props: State) {
+  const {
+    clusters,
+    enabledFeatures,
+    initAttempt,
+    canScheduleUpgrades,
+    showScheduleUpgrade,
+    scheduleUpgradesVisible,
+    hideScheduleUpgrade,
+    onUpdate,
+    upgradeWindows,
+    setSelectedUpgradeWindow,
+    selectedUpgradeWindow,
+    attempt,
+  } = props;
   return (
     <FeatureBox>
-      <FeatureHeader alignItems="center">
+      <FeatureHeader alignItems="center" justifyContent="space-between">
         <FeatureHeaderTitle>Manage Clusters</FeatureHeaderTitle>
+        {canScheduleUpgrades && (
+          <ButtonPrimary onClick={showScheduleUpgrade}>
+            <Clock mr="2" style={{ fontWeight: 'bold' }} />
+            Schedule Upgrades
+          </ButtonPrimary>
+        )}
       </FeatureHeader>
       {initAttempt.status === 'processing' && (
         <Box textAlign="center" m={10}>
@@ -59,6 +85,16 @@ export function Clusters(props: ReturnType<typeof useClusters>) {
             showKubes: enabledFeatures.kubes,
             showDesktops: enabledFeatures.desktops,
           }}
+        />
+      )}
+      {scheduleUpgradesVisible && (
+        <ScheduleUpgrades
+          onSave={onUpdate}
+          onClose={hideScheduleUpgrade}
+          upgradeWindows={upgradeWindows}
+          selectedWindow={selectedUpgradeWindow}
+          onSelectedWindowChange={setSelectedUpgradeWindow}
+          attempt={attempt}
         />
       )}
     </FeatureBox>
