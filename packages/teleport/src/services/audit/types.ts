2eb06470b73c3abd283a6585aa418d139390c585
matheus <matheus.battirola@goteleport.com>
2022-07-14 14:39:41 -0300

    Fix audit event

--- packages/teleport/src/services/audit/types.ts
+++ packages/teleport/src/services/audit/types.ts
@@ -734,7 +734,7 @@ export type RawEvents = {
   [eventCodes.UPGRADE_WINDOW_UPDATED]: RawEvent<
     typeof eventCodes.UPGRADE_WINDOW_UPDATED,
     {
-      upgrade_window: string,
+      upgrade_window_start: string;
     }
   >;
 };
