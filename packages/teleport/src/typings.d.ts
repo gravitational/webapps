c7af3d0016f718c427d5e8befe1dbf9c00e2b370
Ryan Clark <ryan.clark@goteleport.com>
2022-07-15 16:00:59 +0000

    Use `?raw` with asset modules instead of `raw-loader`

--- packages/teleport/src/typings.d.ts
+++ packages/teleport/src/typings.d.ts
@@ -3,8 +3,4 @@ declare module '*.svg';
 declare module '*.jpg';
 declare module '*.gif';
 declare module '*.ico';
-
-declare module 'raw-loader!*' {
-  const contents: string;
-  export = contents;
-}
+declare module '*.yaml?raw';
