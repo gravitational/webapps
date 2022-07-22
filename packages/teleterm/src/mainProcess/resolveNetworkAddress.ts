a947204db3e419a99826bbf0d709b3990192a4da
Rafał Cieślak <rafal.cieslak@goteleport.com>
2022-07-22 14:40:43 +0200

    Wait for UDS gRPC server to start, add tests

--- packages/teleterm/src/mainProcess/resolveNetworkAddress.ts
+++ packages/teleterm/src/mainProcess/resolveNetworkAddress.ts
@@ -1,25 +1,42 @@
 import { ChildProcess } from 'child_process';
 
 const TCP_PORT_MATCH = /\{CONNECT_GRPC_PORT:\s(\d+)}/;
+// Both the shared process and the tshd output the string no matter if TCP or UDS is used as a
+// transport method.
+const UDS_MATCH = /\{CONNECT_GRPC_PORT:/;
 
 export async function resolveNetworkAddress(
   requestedAddress: string,
   process: ChildProcess,
   timeoutMs = 10_000 // 10s
 ): Promise<string> {
-  // UDS
-  if (new URL(requestedAddress).protocol === 'unix:') {
-    return requestedAddress;
-  }
+  const protocol = new URL(requestedAddress).protocol;
 
-  // TCP
-  const matchResult = await waitForMatchInStdout(
-    TCP_PORT_MATCH,
-    requestedAddress,
-    process,
-    timeoutMs
-  );
-  return `localhost:${matchResult[1]}`;
+  switch (protocol) {
+    case 'unix:': {
+      // In case of UDS, we know the address upfront. Still, we wait for the process to emit the
+      // message so that we know the server was started and accepts connections.
+      await waitForMatchInStdout(
+        UDS_MATCH,
+        requestedAddress,
+        process,
+        timeoutMs
+      );
+      return requestedAddress;
+    }
+    case 'tcp:': {
+      const matchResult = await waitForMatchInStdout(
+        TCP_PORT_MATCH,
+        requestedAddress,
+        process,
+        timeoutMs
+      );
+      return `localhost:${matchResult[1]}`;
+    }
+    default: {
+      throw new Error(`Unknown protocol ${protocol}`);
+    }
+  }
 }
 
 function waitForMatchInStdout(
