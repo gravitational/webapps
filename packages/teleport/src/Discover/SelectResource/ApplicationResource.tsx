153ab130ce8d711aac1eb6a29e15626e9e15a771
Ryan Clark <ryan@ryanclark.me>
2022-08-18 13:36:19 +0100

    Add components for each resource types

---
+++ packages/teleport/src/Discover/SelectResource/ApplicationResource.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { Text } from 'design';
+
+import { ActionButtons, TextBox } from 'teleport/Discover/Shared';
+
+interface ApplicationResourceProps {
+  disabled: boolean;
+  onProceed: () => void;
+}
+
+export function ApplicationResource(props: ApplicationResourceProps) {
+  let content;
+  if (props.disabled) {
+    content = <PermissionsErrorMessage />;
+  }
+
+  return (
+    <>
+      {content}
+
+      <ActionButtons
+        onProceed={() => props.onProceed()}
+        disableProceed={props.disabled}
+      />
+    </>
+  );
+}
+
+function PermissionsErrorMessage() {
+  return (
+    <TextBox data-testid="application-permissions-error">
+      <Text typography="h5">
+        You are not able to add new Applications. There are two possible reasons
+        for this:
+      </Text>
+      <ul style={{ paddingLeft: 28 }}>
+        <li>
+          Your Teleport Enterprise license does not include Application Access.
+          Reach out to your Teleport admin to enable Application Access.
+        </li>
+        <li>
+          You don’t have sufficient permissions to add Applications. Reach out to
+          your Teleport admin to request additional permissions.
+        </li>
+      </ul>
+    </TextBox>
+  );
+}
---
+++ packages/teleport/src/Discover/SelectResource/KubernetesResource.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { Text } from 'design';
+
+import { ActionButtons, TextBox } from 'teleport/Discover/Shared';
+
+interface KubernetesResourceProps {
+  disabled: boolean;
+  onProceed: () => void;
+}
+
+export function KubernetesResource(props: KubernetesResourceProps) {
+  let content;
+  if (props.disabled) {
+    content = <PermissionsErrorMessage />;
+  }
+
+  return (
+    <>
+      {content}
+
+      <ActionButtons
+        onProceed={() => props.onProceed()}
+        disableProceed={props.disabled}
+      />
+    </>
+  );
+}
+
+function PermissionsErrorMessage() {
+  return (
+    <TextBox data-testid="kubernetes-permissions-error">
+      <Text typography="h5">
+        You are not able to add new Kubernetes resources. There are two possible reasons for this:
+      </Text>
+      <ul style={{ paddingLeft: 28 }}>
+        <li>Your Teleport Enterprise license does not include Kubernetes Access. Reach out to your Teleport admin to enable Kubernetes Access.</li>
+        <li>You don’t have sufficient permissions to add Kubernetes resources. Reach out to your Teleport admin to request additional permissions.</li>
+      </ul>
+    </TextBox>
+  );
+}
