fed3958d111e8ae75dc733778f1fe42ae64711ab
Yassine Bounekhla <yassine@goteleport.com>
2022-10-13 13:08:19 -0400

    update support ticket url

--- packages/teleport/src/Support/Support.tsx
+++ packages/teleport/src/Support/Support.tsx
@@ -55,7 +55,7 @@ export const Support = ({
             {isEnterprise && (
               <SupportLink
                 title="Create a Support Ticket"
-                url="https://gravitational.zendesk.com/hc/en-us/requests/new"
+                url="https://support.goteleport.com"
               />
             )}
             <SupportLink
