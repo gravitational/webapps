2452fb8363dba5297810933863faefa46c1836d9
Ryan Clark <ryan.clark@goteleport.com>
2022-10-19 14:52:30 +0100

    Only use `optimization` during prod builds for faster development

--- packages/build/webpack/webpack.prod.config.js
+++ packages/build/webpack/webpack.prod.config.js
@@ -33,6 +33,28 @@ module.exports = {
   mode: 'production',
   optimization: {
     ...configFactory.createDefaultConfig().optimization,
+    runtimeChunk: true,
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
     minimize: true,
   },
   module: {
