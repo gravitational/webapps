bd7fc4bcd28ba35d6b53a96f1594cb0a0a31681c
Michael Myers <michael.myers@goteleport.com>
2022-10-27 21:20:34 -0500

    Move resource serverside logic to useServerSideResources hook

--- packages/teleterm/src/ui/DocumentCluster/ClusterResources/Servers/useServers.ts
+++ packages/teleterm/src/ui/DocumentCluster/ClusterResources/Servers/useServers.ts
@@ -13,87 +13,24 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-import { useState, useEffect } from 'react';
-
-import { SortType } from 'design/DataTable/types';
-
-import { AgentFilter, AgentLabel } from 'teleport/services/agents';
-
-import { useAsync } from 'shared/hooks/useAsync';
-import getPageCount, { Page } from 'shared/utils/getPageCount';
-import getStartKey from 'shared/utils/getStartKey';
-import { addAgentLabelToQuery } from 'shared/utils/addAgentLabelToQuery';
-
-import { useClusterContext } from 'teleterm/ui/DocumentCluster/clusterContext';
+import { ResourceKind } from 'teleport/Discover/Shared';
 import { useAppContext } from 'teleterm/ui/appContextProvider';
+import { useServerSideResources } from '../useServerSideResources';
 
 export function useServers() {
   const appContext = useAppContext();
-  const clusterContext = useClusterContext();
-  const [fetchedData, setFetchedData] = useState(getEmptyFetchedDataState());
-  const [page, setPage] = useState<Page>({ keys: [], index: 0 });
-  const [agentFilter, setAgentFilter] = useState<AgentFilter>({
-    sort: getDefaultSort(),
-  });
-  const limit = 15;
 
-  const [fetchAttempt, fetch] = useAsync(async (dir?: 'prev' | 'next') => {
-    const data = await appContext.clustersService.fetchServers({
-      ...agentFilter,
-      clusterUri: clusterContext.clusterUri,
-      startKey: getStartKey({ dir, page }),
-      searchAsRoles: 'no',
-      limit,
-    });
-    setFetchedData({
-      agents: data.agentsList,
-      startKey: data.startKey,
-      totalCount: data.totalCount,
-    });
-    handleSetPage(dir, data.startKey);
-  });
-
-  useEffect(() => {
-    fetch();
-  }, [agentFilter]);
-
-  function handleSetPage(dir: '' | 'prev' | 'next', startKey: string) {
-    switch (dir) {
-      case 'prev':
-        return setPage({
-          keys: page.keys.slice(0, -1),
-          index: page.index - 1,
-        });
-      case 'next':
-        return setPage({
-          keys: [...page.keys, startKey],
-          index: page.index + 1,
-        });
-      default:
-        return setPage({
-          keys: ['', startKey],
-          index: 0,
-        });
-    }
-  }
-
-  function updateSort(sort: SortType) {
-    setAgentFilter({ ...agentFilter, sort });
-  }
-
-  function updateSearch(search: string) {
-    setAgentFilter({ ...agentFilter, query: '', search });
-  }
-
-  function updateQuery(query: string) {
-    setAgentFilter({ ...agentFilter, search: '', query });
-  }
-
-  function onAgentLabelClick(label: AgentLabel) {
-    const query = addAgentLabelToQuery(agentFilter, label);
-    setAgentFilter({ ...agentFilter, search: '', query });
-  }
+  const {
+    fetchAttempt,
+    updateQuery,
+    agentFilter,
+    prevPage,
+    nextPage,
+    updateSearch,
+    onAgentLabelClick,
+    updateSort,
+    pageCount,
+  } = useServerSideResources(ResourceKind.Server);
 
   function getSshLogins(serverUri: string): string[] {
     const cluster = appContext.clustersService.findClusterByResource(serverUri);
@@ -117,35 +54,23 @@ export function useServers() {
 
   return {
     fetchAttempt,
-    servers: fetchedData.agents,
+    servers: fetchAttempt.data?.agentsList,
     getSshLogins,
     connect,
     agentFilter,
     updateQuery,
     updateSearch,
     onAgentLabelClick,
+    pageCount,
     disabledRows: fetchAttempt.status === 'processing',
-    nextPage: page.keys[page.index + 1] ? () => fetch('next') : null,
-    prevPage: page.index > 0 ? () => fetch('prev') : null,
+    nextPage,
+    prevPage,
     customSort: {
       dir: agentFilter.sort?.dir,
       fieldName: agentFilter.sort?.fieldName,
       onSort: updateSort,
     },
-    pageCount: getPageCount({ page, fetchedData, limit }),
   };
-}
-
-function getEmptyFetchedDataState() {
-  return {
-    agents: [],
-    startKey: '',
-    totalCount: 0,
-  };
-}
-
-function getDefaultSort(): SortType {
-  return { fieldName: 'hostname', dir: 'ASC' };
 }
 
 export type State = ReturnType<typeof useServers>;
---
+++ packages/teleterm/src/ui/DocumentCluster/ClusterResources/useServerSideResources.ts
@@ -0,0 +1,153 @@
+import { useState, useEffect } from 'react';
+import { SortType } from 'design/DataTable/types';
+import { useAsync } from 'shared/hooks/useAsync';
+import { ServerSideParams } from 'teleterm/services/tshd/types';
+import { useAppContext } from 'teleterm/ui/appContextProvider';
+import { retryWithRelogin } from 'teleterm/ui/utils';
+import { useClusterContext } from '../clusterContext';
+import { AgentFilter, AgentLabel } from 'teleport/services/agents';
+import { addAgentLabelToQuery } from 'shared/utils/addAgentLabelToQuery';
+import { ResourceKind } from 'teleport/Discover/Shared';
+
+export function useServerSideResources(resource: ResourceKind) {
+  const ctx = useAppContext();
+  const { clusterUri, documentUri } = useClusterContext();
+  const [pageIndex, setPageIndex] = useState(0);
+  const [keys, setKeys] = useState<string[]>([]);
+  const [agentFilter, setAgentFilter] = useState<AgentFilter>({
+    sort: getDefaultSort(),
+  });
+  const [pageCount, setPageCount] = useState({
+    to: 0,
+    from: 0,
+    total: 0,
+  });
+
+  const limit = 15;
+
+  function getFetchCallback(params: ServerSideParams) {
+    switch (resource) {
+      case ResourceKind.Server:
+        return retryWithRelogin(ctx, documentUri, clusterUri, () =>
+          ctx.clustersService.fetchServers(params)
+        );
+      default: {
+        throw new Error(`Fetch not implemented for: ${resource}`);
+      }
+    }
+  }
+
+  // startKey is used here as a way to paginate through agents returned from
+  // their respective rpcs.
+  const [fetchAttempt, fetch] = useAsync(async (startKey: string) => {
+    return getFetchCallback({
+      ...agentFilter,
+      limit,
+      clusterUri,
+      startKey,
+    });
+  });
+
+  // If there is no startKey at the current page's index, there is no more data to get
+  function nextPage() {
+    const proposedKey = keys[pageIndex];
+    if (proposedKey) {
+      return () => {
+        fetch(proposedKey);
+        setPageIndex(pageIndex + 1);
+      };
+    }
+    return null;
+  }
+
+  // If we are at the first page (index 0), we cannot fetch more previous data
+  function prevPage() {
+    const newPageIndex = pageIndex - 1;
+    if (newPageIndex > -1) {
+      return () => {
+        // if we got pageIndex - 1, we'd get the same data as the previous "next" we just called.
+        // we have to go back 2. which is why we use newPageIndex instead
+
+        // example after going from pageIndex = 0 to pageIndex = 1:
+        // ['startKeyFromInitialFetch', 'goEvenFurtherStartKey']
+        //     ^- just came from here          ^- we are here
+        //
+        // so in order to get the start key for the page BEFORE this one, we go back twice.
+        // this could also be written/read as keys[pageIndex - 2]
+        fetch(keys[newPageIndex - 1]);
+        setPageIndex(newPageIndex);
+      };
+    }
+    return null;
+  }
+
+  useEffect(() => {
+    // when agent filter changes we consider it a 'new' search so we should pass an empty
+    // startKey to get the beginning of the list with new filters, and set pageIndex back to zero
+    fetch('');
+    setPageIndex(0);
+  }, [agentFilter]);
+
+  // when we receive data from fetch, we store the startKey (or lack of) according to the current
+  // page index. think of this as "this page's nextKey".
+  // "why don't we just name it nextKey then?"
+  // Mostly because it's called startKey almost everywhere else in the UI, and also because we'd have the same issue
+  // for prevPage if we swapped named, and this comment would be explaining "this page's startKey".
+  useEffect(() => {
+    const newKeys = [...keys];
+    newKeys[pageIndex] = fetchAttempt.data?.startKey;
+    setKeys(newKeys);
+
+    setPageCount(getPageCount());
+  }, [fetchAttempt.data]);
+
+  function updateSort(sort: SortType) {
+    setAgentFilter({ ...agentFilter, sort });
+  }
+
+  function updateSearch(search: string) {
+    setAgentFilter({ ...agentFilter, query: '', search });
+  }
+
+  function updateQuery(query: string) {
+    setAgentFilter({ ...agentFilter, search: '', query });
+  }
+
+  function onAgentLabelClick(label: AgentLabel) {
+    const query = addAgentLabelToQuery(agentFilter, label);
+    setAgentFilter({ ...agentFilter, search: '', query });
+  }
+
+  function getPageCount() {
+    let from = 0;
+    let to = 0;
+
+    let total = fetchAttempt?.data?.totalCount || 0;
+    if (fetchAttempt.status === 'success') {
+      from = pageIndex * limit + 1;
+      to = from + fetchAttempt.data?.agentsList.length - 1;
+    }
+    return {
+      to,
+      total,
+      from,
+    };
+  }
+
+  return {
+    fetchAttempt,
+    fetch,
+    updateSearch,
+    updateSort,
+    updateQuery,
+    agentFilter,
+    prevPage: prevPage(),
+    nextPage: nextPage(),
+    onAgentLabelClick,
+    pageCount,
+  };
+}
+
+function getDefaultSort(): SortType {
+  return { fieldName: 'hostname', dir: 'ASC' };
+}
