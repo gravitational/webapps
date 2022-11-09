12d490581d57f66306d024324a13868a96a4c5a2
Michael Myers <michael.myers@goteleport.com>
2022-11-09 11:44:48 +0900

    Remove unneeded async/await from resourcesService

--- packages/teleterm/src/ui/services/resources/resourcesService.ts
+++ packages/teleterm/src/ui/services/resources/resourcesService.ts
@@ -3,7 +3,7 @@ import { ServerSideParams, TshClient } from 'teleterm/services/tshd/types';
 export class ResourcesService {
   constructor(private tshClient: TshClient) {}
 
-  async fetchServers(params: ServerSideParams) {
-    return await this.tshClient.getServers(params);
+  fetchServers(params: ServerSideParams) {
+    return this.tshClient.getServers(params);
   }
 }
