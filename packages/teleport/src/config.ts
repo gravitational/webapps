cfbadedd45965b826eaefbacfdeee140607f4232
Lisa Kim <lisa@goteleport.com>
2022-11-17 16:03:01 -0800

    [Discover] Refactor DownloadScript Screens (#1367)
    - Refactor join token api handler and useJoinToken
    - Add agent matcher label to join token request
    - Change join token requests fields into its own type
    - Add db script api handler
    - Revert useJoinToken changes (removes runNow) and
      accepts optional agent matcher label
    - Extract reusable logic for polling
    - Reorganize shared stories
    - Refactor Server's DownloadScript (use suspense and fallback)
    - Refactor Kube HelmChart (DownloadScript) with less hacky ways
    - Create DownloadScript for database (tentativly)

--- packages/teleport/src/config.ts
+++ packages/teleport/src/config.ts
@@ -155,6 +155,7 @@ const cfg = {
     trustedClustersPath: '/v1/webapi/trustedcluster/:name?',
 
     joinTokenPath: '/v1/webapi/token',
+    dbScriptPath: '/scripts/:token/install-database.sh',
     nodeScriptPath: '/scripts/:token/install-node.sh',
     appNodeScriptPath: '/scripts/:token/install-app.sh?name=:name&uri=:uri',
 
@@ -274,6 +275,10 @@ const cfg = {
 
   getNodeScriptUrl(token: string) {
     return cfg.baseUrl + generatePath(cfg.api.nodeScriptPath, { token });
+  },
+
+  getDbScriptUrl(token: string) {
+    return cfg.baseUrl + generatePath(cfg.api.dbScriptPath, { token });
   },
 
   getConfigureADUrl(token: string) {
