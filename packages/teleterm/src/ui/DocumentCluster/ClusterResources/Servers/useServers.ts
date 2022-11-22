5cd0d903744a7275a92a8f7e2ffbbfe0abe16af5
Michael Myers <michael.myers@goteleport.com>
2022-11-22 14:00:42 +0900

    Remove unused import

--- packages/teleterm/src/ui/DocumentCluster/ClusterResources/Servers/useServers.ts
+++ packages/teleterm/src/ui/DocumentCluster/ClusterResources/Servers/useServers.ts
@@ -13,8 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import { AttemptStatus } from 'shared/hooks/useAsync';
-
 import { Server, ServerSideParams } from 'teleterm/services/tshd/types';
 import { useAppContext } from 'teleterm/ui/appContextProvider';
 import { makeServer } from 'teleterm/ui/services/clusters';
