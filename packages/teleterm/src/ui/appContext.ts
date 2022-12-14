816e938922d158520ac1570191642b6c31d01874
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2022-12-14 11:59:36 +0100

    Remove lint issue

--- packages/teleterm/src/ui/appContext.ts
+++ packages/teleterm/src/ui/appContext.ts
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { ZodIssue } from 'zod';
-
 import {
   MainProcessClient,
   ElectronGlobals,
