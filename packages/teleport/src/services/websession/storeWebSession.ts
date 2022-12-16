e2be792e7961c9c78f3b0768a6b4653e56166fab
Yassine Bounekhla <yassine@goteleport.com>
2022-12-16 17:24:57 -0500

    fix unused import

--- packages/teleport/src/services/websession/storeWebSession.ts
+++ packages/teleport/src/services/websession/storeWebSession.ts
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import history from 'teleport/services/history';
-
 import { KeysEnum, EventKeys } from './types';
 
 import type { BroadcastChannelMessage, BearerToken } from './types';
