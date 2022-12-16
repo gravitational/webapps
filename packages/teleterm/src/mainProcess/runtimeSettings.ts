4f2353010461d5f21186bfa22ec9034be69706a9
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2022-12-16 12:35:59 +0100

    Add installation ID

--- packages/teleterm/src/mainProcess/runtimeSettings.ts
+++ packages/teleterm/src/mainProcess/runtimeSettings.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import os from 'os';
 import path from 'path';
+import crypto from 'crypto';
 
 import { app } from 'electron';
 
@@ -19,7 +20,7 @@ const TSH_BIN_ENV_VAR = 'CONNECT_TSH_BIN_PATH';
 // We default to webapps/../teleport/build/tsh.
 // prettier-ignore
 const TSH_BIN_DEFAULT_PATH_FOR_DEV = path.resolve(
-  __dirname, '..', '..', '..', '..', '..', '..', '..', 'teleport', 'build', 'tsh'
+  __dirname, '..', '..', '..', '..', '..', '..', '..', 'teleport', 'build', 'tsh',
 );
 
 const dev = env.NODE_ENV === 'development' || env.DEBUG_PROD === 'true';
@@ -73,6 +74,7 @@ function getRuntimeSettings(): RuntimeSettings {
     defaultShell: getDefaultShell(),
     kubeConfigsDir: getKubeConfigsDir(),
     platform: process.platform,
+    installationId: loadInstallationId(),
   };
 }
 
@@ -207,6 +209,34 @@ function getUnixSocketNetworkAddress(socketName: string) {
   }
 
   return `unix://${path.resolve(app.getPath('userData'), socketName)}`;
+}
+
+const UUID_V4_REGEX =
+  /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
+
+function loadInstallationId(): string {
+  const filePath = path.resolve(app.getPath('userData'), 'installation_id');
+  const writeNewId = (): string => {
+    try {
+      const newId = crypto.randomUUID();
+      fs.writeFileSync(filePath, newId);
+      return newId;
+    } catch (error) {
+      throw new Error(
+        `Could not write installation_id to ${filePath}, ${error.message}`
+      );
+    }
+  };
+
+  try {
+    const id = fs.readFileSync(filePath, 'utf-8');
+    if (!UUID_V4_REGEX.test(id)) {
+      return writeNewId();
+    }
+    return id;
+  } catch (error) {
+    return writeNewId();
+  }
 }
 
 export { getRuntimeSettings, getAssetPath };
