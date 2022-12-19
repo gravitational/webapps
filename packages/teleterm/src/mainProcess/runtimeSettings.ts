27a28296d15d3119c2dccdb72c21e2043aa502a5
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2022-12-19 18:39:25 +0100

    Move `loadInstallationId` to a separate file, add tests

--- packages/teleterm/src/mainProcess/runtimeSettings.ts
+++ packages/teleterm/src/mainProcess/runtimeSettings.ts
@@ -1,13 +1,13 @@
 import fs from 'fs';
 import os from 'os';
 import path from 'path';
-import crypto from 'crypto';
 
 import { app } from 'electron';
 
 import Logger from 'teleterm/logger';
 
 import { GrpcServerAddresses, RuntimeSettings } from './types';
+import { loadInstallationId } from './loadInstallationId';
 
 const { argv, env } = process;
 
@@ -74,7 +74,9 @@ function getRuntimeSettings(): RuntimeSettings {
     defaultShell: getDefaultShell(),
     kubeConfigsDir: getKubeConfigsDir(),
     platform: process.platform,
-    installationId: loadInstallationId(),
+    installationId: loadInstallationId(
+      path.resolve(app.getPath('userData'), 'installation_id')
+    ),
   };
 }
 
@@ -209,35 +211,6 @@ function getUnixSocketNetworkAddress(socketName: string) {
   }
 
   return `unix://${path.resolve(app.getPath('userData'), socketName)}`;
-}
-
-const UUID_V4_REGEX =
-  /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
-
-function loadInstallationId(): string {
-  const filePath = path.resolve(app.getPath('userData'), 'installation_id');
-  let id = '';
-  try {
-    id = fs.readFileSync(filePath, 'utf-8');
-  } catch (error) {
-    return writeInstallationId(filePath);
-  }
-  if (!UUID_V4_REGEX.test(id)) {
-    return writeInstallationId(filePath);
-  }
-  return id;
-}
-
-function writeInstallationId(filePath: string): string {
-  const newId = crypto.randomUUID();
-  try {
-    fs.writeFileSync(filePath, newId);
-  } catch (error) {
-    throw new Error(
-      `Could not write installation_id to ${filePath}, ${error.message}`
-    );
-  }
-  return newId;
 }
 
 export { getRuntimeSettings, getAssetPath };
