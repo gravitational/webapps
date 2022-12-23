ddb90fa7c61820bd3be12e44743dc3089225e347
Yassine Bounekhla <yassine@goteleport.com>
2022-12-23 16:35:52 -0500

    address comments

--- packages/teleport/src/Sessions/SessionList/SessionJoinBtn.tsx
+++ packages/teleport/src/Sessions/SessionList/SessionJoinBtn.tsx
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 import React from 'react';
 
 import { ButtonBorder, Text, Box, Menu, MenuItem } from 'design';
@@ -16,13 +32,14 @@ export const SessionJoinBtn = ({
   participantModes: ParticipantMode[];
 }) => {
   // Sorts the list of participantModes so that they are consistently shown in the order of "observer" -> "moderator" -> "peer"
-  const sortedParticipantModes = participantModes.sort((a, b) => {
-    if (a === 'observer') return -1;
-    if (b === 'observer') return 1;
-    if (a === 'moderator') return -1;
-    if (b === 'moderator') return 1;
-    return 0;
-  });
+  const modes = {
+    observer: 1,
+    moderator: 2,
+    peer: 3,
+  };
+  const sortedParticipantModes = participantModes.sort(
+    (a, b) => modes[a] - modes[b]
+  );
 
   return (
     <JoinMenu>
