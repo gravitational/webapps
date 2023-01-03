7f98d1b3673b88a549edd10b8abe0f0dc1c77606
Michael Myers <michael.myers@goteleport.com>
2023-01-03 10:20:30 -0600

    Remove getLoggedInUser function wrapper

--- packages/teleterm/src/ui/hooks/useLoggedInUser.ts
+++ packages/teleterm/src/ui/hooks/useLoggedInUser.ts
@@ -1,5 +1,4 @@
 import { useAppContext } from 'teleterm/ui/appContextProvider';
-import { LoggedInUser } from 'teleterm/services/tshd/types';
 
 export function useLoggedInUser() {
   const ctx = useAppContext();
@@ -7,17 +6,15 @@ export function useLoggedInUser() {
   ctx.clustersService.useState();
   ctx.workspacesService.useState();
 
-  function getLoggedInUser(): LoggedInUser | undefined {
-    const clusterUri = ctx.workspacesService.getRootClusterUri();
-    if (!clusterUri) {
-      return;
-    }
-    const cluster = ctx.clustersService.findCluster(clusterUri);
-    if (!cluster) {
-      return;
-    }
-    return cluster.loggedInUser;
+  const clusterUri = ctx.workspacesService.getRootClusterUri();
+  if (!clusterUri) {
+    return;
   }
 
-  return getLoggedInUser();
+  const cluster = ctx.clustersService.findCluster(clusterUri);
+  if (!cluster) {
+    return;
+  }
+
+  return cluster.loggedInUser;
 }
