f693aa4c32b030f9e9e82880f8805159d818e70a
Grzegorz Zdunek <gzdunek@users.noreply.github.com>
2023-01-04 15:12:27 +0100

    [Connect] Update protofiles to support `authClusterId` (#1483)
    (cherry picked from commit aefa5f63fd6006220b0e54046dcd5e9d3e4d7e01)

--- packages/teleterm/src/ui/TopBar/Identity/Identity.story.tsx
+++ packages/teleterm/src/ui/TopBar/Identity/Identity.story.tsx
@@ -78,6 +78,7 @@ export function OneClusterWithActiveCluster() {
     proxyHost: 'localhost:3080',
     connected: true,
     leaf: false,
+    authClusterId: '73c4746b-d956-4f16-9848-4e3469f70762',
     loggedInUser: {
       activeRequestsList: [],
       name: 'alice',
@@ -173,6 +174,7 @@ export function ManyClustersWithActiveCluster() {
     proxyHost: 'localhost:3080',
     connected: true,
     leaf: false,
+    authClusterId: '73c4746b-d956-4f16-9848-4e3469f70762',
     loggedInUser: {
       activeRequestsList: [],
       name: activeIdentityRootCluster.userName,
@@ -230,6 +232,7 @@ export function LongNamesWithManyRoles() {
     proxyHost: 'localhost:3080',
     connected: true,
     leaf: false,
+    authClusterId: '73c4746b-d956-4f16-9848-4e3469f70762',
     loggedInUser: {
       activeRequestsList: [],
       name: activeIdentityRootCluster.userName,
